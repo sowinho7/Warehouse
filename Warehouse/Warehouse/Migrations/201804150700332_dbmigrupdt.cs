c472517454149c207b0a7433d56fa6ec0bdcab72
sowinho7 <wojteks094@gmail.com>
2018-04-15 12:52:58 +0200

    update ziomky

---
+++ Warehouse/Warehouse/Migrations/201804150700332_dbmigrupdt.cs
@@ -0,0 +1,16 @@
+namespace Warehouse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dbmigrupdt : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
